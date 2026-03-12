91eaf581f122ec79674067d0ab979876831768c6
Judah <judahpang@gmail.com>
2026-03-12 22:11:50 +0800

    Use get instead of map

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -28,7 +28,7 @@ public class JsonAdaptedPersonTest {
     private static final String VALID_PHONE = BENSON.getPhone().toString();
     private static final String VALID_EMAIL = BENSON.getEmail().toString();
     private static final String VALID_ADDRESS = BENSON.getAddress().toString();
-    private static final String VALID_PARENT_NAME = BENSON.getParentName().map(pn -> pn.fullName).orElse(null);
+    private static final String VALID_PARENT_NAME = BENSON.getParentName().get().fullName;
     private static final List<JsonAdaptedTag> VALID_TAGS = BENSON.getTags().stream()
             .map(JsonAdaptedTag::new)
             .collect(Collectors.toList());
